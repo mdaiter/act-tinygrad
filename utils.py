273b48af25d407237f3d6cfc394d55a9a15a4d63
Matthew Daiter <mdaiter8121@gmail.com>
2024-10-22 00:43:34 -0700

    Add fix for metal

--- utils.py
+++ utils.py
@@ -20,14 +20,20 @@ def clip_grad_norm_(parameters, max_norm) -> Tensor:
     if len(parameters) == 0:
         return Tensor(0.0)
     
+    # For Metal, we need this: Metal can only support 32 memory buffers
+    is_32 = 0
     total_norm = Tensor.zeros((), dtype=dtypes.float32)
     for p in parameters:
         if p.grad is not None:
             param_norm = p.grad.flatten().square().sum().realize()
             total_norm += param_norm
-            total_norm = total_norm.contiguous().realize()
+            is_32 += 1
+            if is_32 > 29:
+                total_norm = total_norm.realize()
+                is_32 = 0
     
-    total_norm = (total_norm + 1e-12).sqrt()
+    # We need one extra realize here: make sure there's nothing left in the parameter calc
+    total_norm = (total_norm.realize() + 1e-12).sqrt()
     print(f'total_norm: {total_norm.numpy()}')
     clip_coef = max_norm / (total_norm + 1e-6)
     clip_coef = Tensor.minimum(clip_coef, Tensor.ones_like(clip_coef)).contiguous().realize().item()
